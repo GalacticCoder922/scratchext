2e6e97d1f40dcadd7465b1fd0e13fb77d640f4ea
Aiden <132232895+GalacticCoder922@users.noreply.github.com>
2023-06-05 06:28:44 -0400

    Remove comment

--- truefalse.js
+++ truefalse.js
@@ -10,7 +10,7 @@ class TrueFalse {
                     }
                 },
             }],
-        "menus": { //we will get back to this in a later tutorial
+        "menus": {
         }
     };
     substringy {
